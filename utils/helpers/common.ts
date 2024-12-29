0073398bba176167816661cbc1b1edbc6a8cd0ab
Said Gandur <said.gandur@coxautoinc.com>
2024-12-28 21:45:34 -0500

    add common utils

--- utils/helpers/common.ts
+++ utils/helpers/common.ts
@@ -1,27 +1,129 @@
 import { EUser } from "../enum";
 
-export const roleMapFromToken = (role: number) : EUser | null=>{
-
+export const roleMapFromToken = (role: number): EUser | null => {
   switch (role) {
     case 1:
-      return EUser.SUPER_ADMIN
+      return EUser.SUPER_ADMIN;
     case 2:
-      return EUser.ADMIN
+      return EUser.ADMIN;
     case 3:
-      return EUser.ADMIN_COMPANY
+      return EUser.ADMIN_COMPANY;
     case 4:
-      return EUser.COMPANY
+      return EUser.COMPANY;
     default:
-      return null
+      return null;
   }
+};
+
+export const isValidEmail = (value: any):boolean =>{
+  if(!value) return false
+  if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) return false
+  return true
 }
 
-export const isValidToken = (token:any) => {
+export const getCreateCompanyPayload = (data: any) => {
+  const formattedPayload = {
+    employees: parseInt(data.employees.split("-")[1]) || 0,
+    sector: data.sector,
+    city: data.city,
+    name: data.company_name,
+    konempleo_responsible: data.konempleo_contact,
+    responsible_user: {
+      fullname: data.user_company_name,
+      email: data.user_company_email,
+      role: 3,
+      phone: data.user_company_phone,
+    },
+    document: data.document_number,
+    availableoffers: data.company_offers, // same according to julian
+    totaloffers: data.company_offers, // same according to julian
+    activeoffers: 0,
+    document_type: data.company_document_type,
+  };
+  return formattedPayload;
+};
+
+export const getCreateUserPayload = (user: any, isRecruiter: boolean = false) => {
+  if (isRecruiter) {
+    return {
+      email: user.email,
+      fullname: user.name,
+      phone: user.phone,
+      role: 4 
+    }
+  }
+  return {
+    user_in: {
+      email: user.email,
+      fullname: user.name,
+      phone: user.phone,
+      role: user.role,
+    },
+    company_ids: user.companies
+  };
+};
+
+type TCompanyPUT = {company: any, KOEUserId: number, companyUser?:any, updateActive?: boolean, updateDeleted?: boolean,}
+
+export const getPUTCompanyPayload = ({company, updateActive, updateDeleted, KOEUserId, companyUser }: TCompanyPUT ) => {
+  const payload = {
+    name: company.name,
+    sector: company.sector,
+    document: company.document,
+    document_type: company.document_type,
+    city: company.city,
+    employees: Number(company.employees) ? company.employees : parseInt(company.employees.split("-")[1]),
+    availableoffers: company.availableoffers,
+    totaloffers: company.totaloffers,
+    konempleo_responsible: KOEUserId,
+    responsible_user: {
+      fullname: companyUser?.fullname || company.recruiter_name,
+      email: companyUser?.email || company.recruiter_email,
+      phone: companyUser?.phone || company.phone,
+    },
+    is_deleted: company.is_deleted,
+    active: company.active
+  };
+  if(updateActive){
+    payload.active = !company.active
+    return payload
+  } else if(updateDeleted) {
+    payload.is_deleted = !company.is_deleted
+    return payload
+  }
+
+  return payload
+};
+
+type TUsersPUT = {user: any, updateActive?: boolean, updateDeleted?: boolean,}
+
+export const getPUTUserPayload = ({user, updateActive, updateDeleted }: TUsersPUT ) => {
+  const payload = {
+    user_in:{
+      fullname: user.fullname,
+      phone: user.phone,
+      email:user.email,
+      is_deleted: user.is_deleted,
+      active: user.active
+    },
+    company_ids: user.companies
+  };
+  if(updateActive){
+    payload.user_in.active = !user.active
+    return payload
+  } else if(updateDeleted) {
+    payload.user_in.is_deleted = !user.is_deleted
+    return payload
+  }
+  return payload
+};
+
+export const isValidToken = (token: any) => {
   const currentTimeStamp = Math.floor(Date.now() / 1000);
   const isExpired = currentTimeStamp > token.exp;
   if (isExpired) return false;
-  return true
-}
+  return true;
+};
 
 export const getUserHeaderText = (userRole: EUser | null) => {
   switch (userRole) {
@@ -52,7 +154,7 @@ export const getUserRoleNavMapping = (userRole: EUser) => {
           redirect: "/admin/offer-details",
         },
         {
-          label: "Lista de ofertas",
+          label: "Detalle de Ofertas",
           icons: ["fas", "list-check"],
           redirect: "/admin/offers",
         },
@@ -108,7 +210,7 @@ export const getUserRoleNavMapping = (userRole: EUser) => {
           redirect: "/super-admin/offer-details",
         },
         {
-          label: "Lista de ofertas",
+          label: "Detalle de Ofertas",
           icons: ["fas", "list-check"],
           redirect: "/super-admin/offers",
         },
