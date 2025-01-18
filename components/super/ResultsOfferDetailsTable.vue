af71f9ecb6ff62d22ec6c3b3fcab135edc55f208
Said Gandur <said.gandur@coxautoinc.com>
2025-01-18 10:34:16 -0500

    Add offer details integration for super

--- components/super/ResultsOfferDetailsTable.vue
+++ components/super/ResultsOfferDetailsTable.vue
@@ -60,33 +60,124 @@
             <tr>
               <th>Ranking</th>
               <th>Nombre</th>
-              <th>Cédula</th>
-              <th>Ciudad</th>
-              <th>Habilidades encontradas</th>
-              <th>Género</th>
-              <th>Móvil</th>
+              <th>Whatsapp</th>
+              <th>Revisar tus datos</th>
+              <th>Tus Datos</th>
+              <th>Movil</th>
               <th>Mail</th>
               <th>Score</th>
-              <th>Experiencia (en años)</th>
-              <th>Tiempo promedio en cada trabajo</th>
-              <th>Nivel Educativo</th>
+              <th>Contratado</th>
+              <th>Comentarios</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="(result, index) in paginatedResults" :key="index">
-              <td class="ranking">{{ result.vitae_offer_id }}</td>
-              <td>{{ result.candidate_name }}</td>
-              <td>{{ result.dni }}</td>
-              <td>{{ result.city }}</td>
-              <!-- <td>{{ result.skills.join(", ") }}</td> -->
-              <td>skills</td>
-              <td>{{ result.gender }}</td>
+              <td class="ranking">{{ index + 1 }}</td>
+              <td>
+                <div class="icon" @click="onCandidateClick(result)">
+                  {{ result.candidate_name }}
+                </div>
+              </td>
+              <td>
+                <div
+                  v-if="
+                    result.whatsapp_status === 'notsent' ||
+                    result.whatsapp_status === null
+                  "
+                  class="tooltip"
+                >
+                  <font-awesome-icon
+                    class="icon"
+                    :icon="['fab', 'whatsapp']"
+                    size="xl"
+                    style="color: #19e668"
+                    @click="onWhatsappClick(result)"
+                  />
+                  <span class="tooltiptext">Notificar via Whatsapp</span>
+                </div>
+                <div v-else-if="result.whatsapp_status === 'pending_response'">
+                  Enviado
+                </div>
+                <div v-else>
+                  {{ result.whatsapp_status }}
+                </div>
+              </td>
+              <td>
+                <div v-if="result.background_check !== null" class="tooltip">
+                  <font-awesome-icon
+                    :icon="['fas', 'pen-to-square']"
+                    :style="{ color: 'gray' }"
+                  />
+                  <span class="tooltiptext">Antecedentes consultados</span>
+                </div>
+                <div v-else class="tooltip">
+                  <font-awesome-icon
+                    class="icon"
+                    :icon="['fas', 'pen-to-square']"
+                    :style="{ color: '#19e668' }"
+                    @click="onBackgroundCheck(result.vitae_offer_id)"
+                  />
+                  <span class="tooltiptext">Verificar antecedentes</span>
+                </div>
+              </td>
+              <td>
+                <div v-if="result.background_check === null">
+                  <span>Sin Revisar</span>
+                </div>
+                <div
+                  v-else-if="result.background_check === 'true'"
+                  class="bg-check negative"
+                >
+                  <span>Con hallazgos</span>
+                </div>
+                <div v-else class="bg-check positive">
+                  <span>Sin Antecedentes</span>
+                </div>
+              </td>
               <td>{{ result.candidate_phone }}</td>
               <td>{{ result.candidate_mail }}</td>
               <td>{{ result.response_score }}</td>
-              <td>{{ result.average_experience_in_years }}</td>
-              <td>{{ result.average_time_per_job }}</td>
-              <td>{{ result.education_level }}</td>
+              <td>
+                <div class="tooltip">
+                  <label class="toggle-label">
+                    <input
+                      type="checkbox"
+                      class="toggle-box"
+                      :true-value="'hired'"
+                      :false-value="'pending'"
+                      @change="onToggleChange(result)"
+                    />
+                    <div
+                      :class="[
+                        'toggle-div',
+                        { checked: result.status === 'hired' },
+                      ]"
+                    >
+                      <span class="after-element" />
+                    </div>
+                  </label>
+                  <span class="tooltiptext">Marcar como Contratado</span>
+                </div>
+              </td>
+              <td>
+                <div class="comments-wrapper">
+                  <span class="comment-content">
+                    {{ result.comments }}
+                  </span>
+                  <div class="tooltip">
+                    <font-awesome-icon
+                      class="icon"
+                      :icon="['far', 'comment-dots']"
+                      :style="{ color: '#5C60F5' }"
+                      @click="handleShowModal(result)"
+                    />
+                    <span v-if="!!result.comments" class="tooltiptext"
+                      >Actualizar Comentario</span
+                    >
+                    <span v-else class="tooltiptext">Agregar Comentario</span>
+                  </div>
+                </div>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -111,18 +202,36 @@
       <span>Ningun candidato hace match con tu busqueda</span>
     </div>
   </div>
+  <CoreModal
+    :show="showModal"
+    title="Añadir Comentario"
+    placeholder="Escribre tu comentario aqui"
+    primary-button-text="Agregar"
+    secondary-button-text="Cancelar"
+    @close="() => (showModal = false)"
+    @submit="onComment"
+  />
 </template>
-
 <script lang="ts" setup>
 import type { ISuperCandidatesTableRow } from "~/utils/interfaces";
 import { useUserStore } from "~/store/user.store";
 import { useHelperStore } from "~/store/helper.store";
 
+interface IOfferData {
+  zone: string;
+  salary: string;
+  contract: string;
+  offerName: string;
+  companyName: string;
+}
 interface ITableProps {
   offerId: string;
+  requestOfferData: (id: string) => IOfferData | undefined;
 }
+
 const props = withDefaults(defineProps<ITableProps>(), {
   offerId: "",
+  requestOfferData: () => undefined,
 });
 const { $toast } = useNuxtApp();
 const userStore = useUserStore();
@@ -132,13 +241,16 @@ const results = ref<ISuperCandidatesTableRow[]>([]);
 const filteredResults = ref<ISuperCandidatesTableRow[]>([]);
 const currentPage = ref(1);
 const rowsPerPage = ref(10);
+const showModal = ref<boolean>(false);
+const selectedCandidate = ref<ISuperCandidatesTableRow>();
 
 const onHandleCandidateSearch = (searchValue: string): void => {
   filteredResults.value = results.value.filter((item) => {
     const name = item.candidate_name?.toLowerCase(); // optional chaining until backend gets fixed
     return name.includes(searchValue.toLowerCase());
   });
-  currentPage.value = 1;};
+  currentPage.value = 1;
+};
 
 // Computed property to calculate the total number of pages
 const totalPages = computed(() => {
@@ -164,6 +276,168 @@ const previousPage = () => {
   }
 };
 
+const onCandidateClick = async (candidate: ISuperCandidatesTableRow) => {
+  if (candidate.url) {
+    // temporary => it is supposed that every candidate should have a valid url
+    const candidatePath = candidate.url.split(".com/")[1];
+    const params: fetchWrapperProps = {
+      method: EFetchMethods.GET,
+      path: `generate-presigned-url/?file_path=${candidatePath}`,
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const { data, error } = await useFetchWrapper(params);
+    if (error.value) {
+      helperStore.renderToastMessage($toast, true, {
+        error: "something went wrong bringing CV",
+      });
+    } else {
+      helperStore.renderToastMessage($toast, false, {
+        success: "Abriendo CV del candidato",
+      });
+      setTimeout(() => {
+        window.open(data.value.url);
+      }, 2000);
+    }
+  }
+};
+
+const onWhatsappClick = async (candidate: ISuperCandidatesTableRow) => {
+  const offerData = props.requestOfferData(props.offerId);
+  const payload = {
+    candidate_phone: candidate.candidate_phone,
+    candidate_name: candidate.candidate_name,
+    offer_name: offerData?.offerName,
+    zone: offerData?.zone,
+    salary: offerData?.salary,
+    contract: offerData?.contract,
+    offerId: Number(props.offerId),
+    vitae_offer_id: candidate.vitae_offer_id,
+  };
+
+  const params: fetchWrapperProps = {
+    method: EFetchMethods.POST,
+    path: `cvoffers/send-message/`,
+    body: JSON.stringify(payload),
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const { data, error } = await useFetchWrapper(params);
+  if (error.value) {
+    helperStore.renderToastMessage($toast, true, {
+      error: "something went wrong sending the message",
+    });
+  } else {
+    helperStore.renderToastMessage($toast, false, {
+      success: "Mensaje de Whatsapp enviado exitosamente",
+    });
+    console.log(data.value);
+  }
+};
+
+const onBackgroundCheck = async (vitaeId: number) => {
+  const params: fetchWrapperProps = {
+    method: EFetchMethods.GET,
+    path: `background-check/${vitaeId}`,
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const { data, error } = await useFetchWrapper(params);
+  if (error.value) {
+    helperStore.renderToastMessage($toast, true, {
+      error: "something went wrong checking candidate background",
+    });
+  } else {
+    helperStore.renderToastMessage($toast, false, {
+      success: "Antecedentes consultados correctamente",
+    });
+    console.log(data.value);
+  }
+};
+
+const onComment = async (comment: string) => {
+  if (selectedCandidate.value) {
+    const params: fetchWrapperProps = {
+      method: EFetchMethods.PUT,
+      path: `cvoffers/${selectedCandidate.value.vitae_offer_id}/status`,
+      body: JSON.stringify({
+        status: selectedCandidate.value.status,
+        comments: comment,
+      }),
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const { data, error } = await useFetchWrapper(params);
+    if (error.value) {
+      helperStore.renderToastMessage($toast, true, {
+        error: "something went wrong Updating candidate comment",
+      });
+      showModal.value = false;
+    } else {
+      helperStore.renderToastMessage($toast, false, {
+        success: "Comentario actualizado correctamente",
+      });
+      showModal.value = false;
+      const updatedIndex = results.value.findIndex(
+        (item) => item.vitae_offer_id === selectedCandidate.value.vitae_offer_id
+      );
+      if (updatedIndex >= 0) {
+        results.value[updatedIndex] = {
+          ...results.value[updatedIndex],
+          comments: data.value.comments,
+        };
+      }
+    }
+  }
+};
+
+const handleShowModal = (candidate: ISuperCandidatesTableRow) => {
+  showModal.value = true;
+  selectedCandidate.value = candidate;
+};
+
+const onToggleChange = async (candidate: ISuperCandidatesTableRow) => {
+  const params: fetchWrapperProps = {
+    method: EFetchMethods.PUT,
+    path: `cvoffers/${candidate.vitae_offer_id}/status`,
+    body: JSON.stringify({
+      status: candidate.status === "pending" ? "hired" : "pending",
+      comments: candidate.comments,
+    }),
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const { data, error } = await useFetchWrapper(params);
+  if (error.value) {
+    helperStore.renderToastMessage($toast, true, {
+      error: "something went wrong Updating candidate status",
+    });
+  } else {
+    helperStore.renderToastMessage($toast, false, {
+      success: "Status actualizado correctamente",
+    });
+    const updatedIndex = results.value.findIndex(
+      (item) => item.vitae_offer_id === candidate.vitae_offer_id
+    );
+    if (updatedIndex >= 0) {
+      results.value[updatedIndex] = {
+        ...results.value[updatedIndex],
+        status: data.value.status,
+      };
+    }
+  }
+};
+
 const fetchOfferDetails = async (offerId: number) => {
   const params: fetchWrapperProps = {
     method: EFetchMethods.GET,
@@ -180,19 +454,31 @@ const fetchOfferDetails = async (offerId: number) => {
     });
     results.value = [];
   } else {
-    results.value = data.value;
-    filteredResults.value = data.value;
+    const mappedResponse = data.value.sort(
+      (a, b) => b.response_score - a.response_score
+    );
+    results.value = mappedResponse;
+    filteredResults.value = mappedResponse;
   }
 };
 
+onMounted(async () => {
+  await fetchOfferDetails(Number(props.offerId));
+});
+
 watch(
   () => props.offerId,
-  async (newOfferId: string) => {
-    if (newOfferId) await fetchOfferDetails(Number(newOfferId));
+  async (newOfferId) => {
+    if (newOfferId === "") {
+      // reset values to default
+      results.value = [];
+      filteredResults.value = [];
+    } else {
+      await fetchOfferDetails(Number(newOfferId));
+    }
   }
 );
 </script>
-
 <style lang="scss" scoped>
 .results-table {
   .kpi-section {
@@ -244,7 +530,7 @@ watch(
       }
 
       tbody tr:nth-child(2n) {
-        background-color: #f9fafb; /* Adjust this color to your needs */
+        background-color: #f9fafb;
       }
 
       th:first-child {
@@ -268,6 +554,73 @@ watch(
         vertical-align: middle;
         padding: 1.5rem 2rem;
       }
+
+      .bg-check {
+        font-weight: 600;
+
+        &.negative {
+          color: red;
+        }
+        &.positive {
+          color: green;
+        }
+      }
+
+      .toggle-label {
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+      }
+
+      .toggle-box {
+        position: absolute;
+        width: 1px;
+        height: 1px;
+        padding: 0;
+        margin: -1px;
+        overflow: hidden;
+        clip: rect(0, 0, 0, 0);
+        white-space: nowrap;
+        border-width: 0;
+      }
+
+      .toggle-div {
+        position: relative;
+        width: 3rem;
+        height: 1.75rem;
+        background-color: red;
+        border-radius: 99px;
+        transition: background-color 0.3s;
+
+        &.checked {
+          background-color: #0d9488;
+        }
+
+        .after-element {
+          content: "";
+          position: absolute;
+          top: 0.1rem;
+          left: 0.15rem;
+          width: 1.5rem;
+          height: 1.5rem;
+          background-color: #ffffff;
+          border: 1px solid #d1d5db;
+          border-radius: 9999px;
+          transition: transform 0.3s ease;
+        }
+
+        &.checked .after-element {
+          transform: translateX(1.25rem);
+        }
+      }
+
+      .comments-wrapper {
+        display: flex;
+        gap: 8px;
+        justify-content: center;
+        align-items: center;
+      }
+
       .avatar {
         height: 30px;
         width: 30px;
@@ -300,11 +653,15 @@ watch(
       .tooltip {
         position: relative;
         display: inline-block;
+
+        .icon {
+          cursor: pointer;
+        }
       }
 
       .tooltip .tooltiptext {
         visibility: hidden;
-        width: 80px;
+        width: 100px;
         background-color: #333;
         color: #fff;
         text-align: center;
