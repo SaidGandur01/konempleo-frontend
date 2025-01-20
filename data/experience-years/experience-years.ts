bc519c0003d58f73854aa48ac1376b70fe8016d2
Said Gandur <said.gandur@coxautoinc.com>
2025-01-20 10:51:10 -0500

    update offer dropdowns

---
+++ data/experience-years/experience-years.ts
@@ -0,0 +1,8 @@
+export const workExperienceData: Array<{ key: string; value: string }> = [
+  { key: "1", value: "Sin Experiencia" },
+  { key: "2", value: "6 meses" },
+  { key: "3", value: "1 año" },
+  { key: "4", value: "2 años" },
+  { key: "5", value: "3 años" },
+  { key: "5", value: "Mas de 3 años" }
+];
--- data/work-type/work-type.ts
+++ data/work-type/work-type.ts
@@ -1,7 +1,5 @@
 export const workTypeListData: Array<{ key: string; value: string }> = [
-  { key: "1", value: "Work Type 1" },
-  { key: "2", value: "Work Type 2" },
-  { key: "3", value: "Work Type 3" },
-  { key: "4", value: "Work Type 4" },
-  { key: "5", value: "Work Type 5" }
+  { key: "1", value: "Presencial" },
+  { key: "2", value: "Remoto" },
+  { key: "3", value: "Hibrido" },
 ];
