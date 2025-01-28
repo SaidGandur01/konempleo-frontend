f1b5d402479e19a7994a906dd3d699e562065a12
Said Gandur <said.gandur@coxautoinc.com>
2025-01-27 19:59:52 -0500

    fix changes due backend changing again

--- components/company/ResultsOffersTable.vue
+++ components/company/ResultsOffersTable.vue
@@ -80,7 +80,7 @@
                 {{ result.name }}
               </td>
               <td>{{ result.vitae_offer_count }}</td>
-              <td>{{ result.smartdataId_count }}</td>
+              <td>{{ result.contacted }}</td>
               <td>{{ result.background_check_count }}</td>
               <td>{{ result.assigned_cvs }}</td>
               <td>
@@ -171,8 +171,8 @@ const kpiData = computed(() => {
     return data.reduce(
       (acc, item, index, arr) => {
         acc.totalCandidates += item.vitae_offer_count;
-        acc.contacted += item.smartdataId_count;
-        acc.interested += item.interested_count;
+        acc.contacted += item.contacted;
+        acc.interested += item.interested;
         if (index === arr.length - 1) {
           acc.totalOffers = arr.length;
           acc.egc = (acc.contacted / acc.totalCandidates) * 100;
